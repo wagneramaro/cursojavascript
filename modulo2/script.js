6496b7d509d6fc6d169700e5350c63761ae3d611
Wagner Amaro (Notebook) <contato@agenciawide.com.br>
2021-01-16 07:12:46 -0300

    update module

--- modulo2/script.js
+++ modulo2/script.js
@@ -4,3 +4,25 @@ titulo.innerHTML = "Novo Título alterado pelo JavaScript";
 
 var input = document.getElementById("campo");
 input.value = "Novo conteúdo do input";
+
+//FUNCOES
+function alterar(){
+    document.getElementById("trocar").innerHTML = "Texto alterado pela função";
+}
+
+function somar(x, y){
+    let total = x + y;
+
+    return total;
+}
+
+var resultado = somar(5, 94);
+console.log(resultado);
+
+//eventos de teclado
+function digitou(e){
+    if(e.key == 13 && e.ctrlKey == true){
+        let texto = document.getElementById("inputdeteste").value;
+        console.log(texto);
+    }
+}
